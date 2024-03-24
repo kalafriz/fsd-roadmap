c6f95e879cb4c0aa345ac9073f8fa4cb03a8cf9b
kalafriz <kalafriz@bu.edu>
2024-03-23 23:06:52 -0400

    Added JavaScript
    Added basic JavaScript  alert functions

---
+++ basic-html-css-js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// Script used by simple-page.html
+alert("The script for this alert is in a separate file.");
+alert("Another one!");
+
+let message = 'This message is stored in a variable.';
+alert(message);
+
+let user = 'John', age = 25;
+const bday = '18.03.2995';
+
+const COLOR_BLUE="00F"
+alert(`${user} is ${age} years old.`)
