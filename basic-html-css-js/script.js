0c83a6f435f6bc3c83fed226ffe7405292a10826
kalafriz <kalafriz@bu.edu>
2024-03-25 06:18:14 -0400

    debug age alerts ReferenceError
    Moved ageDiff declaration to fix ReferenceError on confirm alert

--- basic-html-css-js/script.js
+++ basic-html-css-js/script.js
@@ -1,14 +1,30 @@
 "use strict";
 
 // Script used by simple-page.html
-alert("The script for this alert is in a separate file.");
-alert("Another one!");
-
-let message = 'This message is stored in a variable.';
-alert(message);
 
 let user = 'John', age = 25;
 const bday = '18.03.2995';
 
 const COLOR_BLUE="00F"
-alert(`${user} is ${age} years old.`)
+alert(`${user} is ${age} years old.`);
+
+let yourAge = prompt("How old are you?", ["50"]);
+
+let ageDiff= (age1, age2) => Math.abs(age1-age2);
+if(confirm(`Are you really ${yourAge} years old?!`)){
+
+    let message = (yourAge > age) ? `You're ${ageDiff(age, yourAge)} years older than ${user}!`:
+        (yourAge==age) ? `You're the same age as ${user}!`:
+        `You're ${ageDiff(age, yourAge)} years younger than ${user}!`;
+    alert(message);
+}
+
+
+function countToThree(){
+    let i=1;
+    alert("Let's count to 3!");
+    while(i<4){
+        alert(i);
+        i++;
+    }
+}
