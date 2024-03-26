aa6450616645a3e0452233dcb5d77a91dc44fcdf
kalafriz <kalafriz@bu.edu>
2024-03-26 06:03:52 -0400

    added simple graphics
    Added canvas with simple JS graphics and SVG star

--- basic-html-css-js/script.js
+++ basic-html-css-js/script.js
@@ -42,3 +42,29 @@ function removeFancy(){
 function changeText(){
     document.getElementById("changeMe").innerHTML="Ouch!";
 }
+
+function fillCanvas(){
+        
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+
+    // Create gradient
+    var grd = ctx.createLinearGradient(0, 0, 200, 0);
+    grd.addColorStop(0, "red");
+    grd.addColorStop(1, "white");
+
+    // Fill with gradient
+    ctx.fillStyle = grd;
+    ctx.fillRect(10, 10, 150, 80);
+
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+        
+    ctx.beginPath();
+    ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    ctx.font = "30px Arial";
+    ctx.strokeText("Hello World", 10, 50);
+}
