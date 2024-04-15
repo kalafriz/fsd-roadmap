a1eb6ab9f020ba877f751e7f309cfd436c8a04f2
kalafriz <kalafriz@bu.edu>
2024-04-15 18:07:09 -0400

    add button functions setup
    Added initial setup for button functions with alerts.

---
+++ calculator-app/app.js
@@ -0,0 +1,29 @@
+let exp = "";
+
+function operator(op) {
+  alert(op);
+}
+
+function digit(x) {
+  alert(x);
+}
+
+function eval() {
+  alert("=");
+}
+
+function clr() {
+  alert("clear");
+}
+
+function sign() {
+  alert("+/-");
+}
+
+function togglePercent() {
+  alert("percent toggle");
+}
+
+function decimal() {
+  alert("decimal");
+}
