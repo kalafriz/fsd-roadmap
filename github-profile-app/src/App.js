bd11701b3e107b55cc507c8360b9b1745ddb1a1a
kalafriz <kalafriz@bu.edu>
2024-04-15 05:26:47 -0400

    added form setup + favicon

--- github-profile-app/src/App.js
+++ github-profile-app/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // useState
+  const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form className="form">
+        <input
+          className="input"
+          value={username} // retrieved from useState hook call: username is a state retrieved with useState
+          placeholder="GitHub Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button className="button" onClick={handleSubmit}>
+          {loading ? "Searching..." : "Search"}
+        </button>
+      </form>
     </div>
   );
 }
