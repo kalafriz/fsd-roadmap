d8c361bc2665c7f07f9a65c30a4c8648c0a98543
kalafriz <kalafriz@bu.edu>
2024-04-15 05:51:22 -0400

    implement username search + repo loading

--- github-profile-app/src/App.js
+++ github-profile-app/src/App.js
@@ -1,14 +1,44 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import axios from "axios";
 
 function App() {
   // useState
   const [username, setUsername] = useState("");
   const [loading, setLoading] = useState(false);
+  const [repos, setRepos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault(); // Prevent browser from executing full-page form submit
+    searchRepos(); // , instead handle it w React
+  }
+
+  function searchRepos() {
+    setLoading(true);
+    axios({
+      method: "get",
+      url: `https://api.github.com/users/${username}/repos`, // returns array of all repos in user's account
+    }).then((result) => {
+      setLoading(false);
+      setRepos(result.data);
+    });
+  }
+
+  function renderRepo(repo) {
+    return (
+      <div className="row" key={repo.id}>
+        <h2 className="repo-name">
+          {
+            repo.name // from GitHub API
+          }
+        </h2>
+      </div>
+    );
+  }
 
   return (
-    <div className="container">
+    <div className="page-container">
       <form className="form">
         <input
           className="input"
@@ -20,6 +50,11 @@ function App() {
           {loading ? "Searching..." : "Search"}
         </button>
       </form>
+      <div className="results-container">
+        {
+          repos.map(renderRepo) // iterate w .map, for every repo in repos, call renderRepo(repo)
+        }
+      </div>
     </div>
   );
 }
