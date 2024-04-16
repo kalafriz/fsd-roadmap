e3853edc158f88d9d3fd3f8b968e97e57a88f6ed
kalafriz <kalafriz@bu.edu>
2024-04-15 20:26:00 -0400

    Update operator functionality

--- calculator-app/app.js
+++ calculator-app/app.js
@@ -1,27 +1,89 @@
 let total = 0;
 let myNum = "";
+let myOp = "";
+
+let allClear = true;
 
 const inputDisplay = document.getElementById("input");
+const clearBtn = document.getElementById("clear-button");
 
-function updateDisplay() {
-  inputDisplay.innerHTML = myNum;
+function updateDisplay(x) {
+  inputDisplay.innerHTML = x ? x : "0";
+}
+
+function updateNum(x) {
+  myNum = x;
+  updateDisplay(myNum);
+}
+
+function updateTotal(x) {
+  total = x;
+  alert(`total = ${total}`);
+}
+
+function updateClear(state) {
+  allClear = state;
+  clearBtn.innerHTML = allClear ? "AC" : "C";
+}
+
+function updateOp(op) {
+  myOp = op;
 }
 
 function operator(op) {
-  alert(op);
+  eval();
+  updateOp(op);
+  myNum = "";
+}
+
+function doOp(op, t, n) {
+  let res = 0;
+  x = parseFloat(t);
+  y = parseFloat(n);
+  switch (op) {
+    case "+":
+      res = x + y;
+      break;
+    case "-":
+      res = x - y;
+      break;
+    case "*":
+      res = x * y;
+      break;
+    case "/":
+      res = x / y;
+      break;
+    default:
+      console.log("Unknown operator!");
+  }
+
+  alert(`Result = ${res}`);
+  return res;
 }
 
 function digit(x) {
-  myNum = myNum.length < 9 ? myNum.concat(x) : myNum;
-  updateDisplay();
+  updateNum(myNum.length < 9 ? myNum.concat(x) : myNum);
+  updateClear(false);
 }
 
 function eval() {
-  alert("=");
+  if (myOp) {
+    updateTotal(doOp(myOp, total, myNum));
+    updateDisplay(total);
+  } else {
+    updateTotal(myNum);
+    updateDisplay(total);
+  }
 }
 
 function clr() {
-  alert("clear");
+  if (allClear) {
+    updateTotal(0);
+    updateNum("");
+  } else {
+    updateNum("");
+    updateClear(true);
+  }
 }
 
 function sign() {
