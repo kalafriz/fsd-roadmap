af228240f52c46ed5f922a9c4f165728643337f7
kalafriz <kalafriz@bu.edu>
2024-04-22 05:13:10 -0400

    update react functions demo
    (30:24) React function vs vanilla JS comparison demo

--- react/learn-react-scrimba/fun-facts/index.js
+++ react/learn-react-scrimba/fun-facts/index.js
@@ -1,1 +1,15 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
+function MainContent() {
+  return <p>I'm learning React!</p>;
+}
+ReactDOM.render(
+  <div>
+    <MainContent />
+  </div>,
+
+  document.getElementById("root")
+);
+
+const myElement = document.createElement("h1");
+myElement.textContent = "This is an imperative way to program";
+myElement.className = "header";
+document.getElementById("root").append(myElement);
