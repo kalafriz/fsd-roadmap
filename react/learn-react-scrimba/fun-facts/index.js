08f66d7e32c8ba5cabcb61d63942ad14861dfbae
kalafriz <kalafriz@bu.edu>
2024-04-22 05:25:47 -0400

    update JSX demo
    (45:15) another JSX demo

--- react/learn-react-scrimba/fun-facts/index.js
+++ react/learn-react-scrimba/fun-facts/index.js
@@ -1,20 +1,12 @@
-function MainContent() {
-  return <p>I'm learning React!</p>;
-}
-
-const myElement = document.createElement("h1");
-myElement.textContent = "This is an imperative way to program";
-myElement.className = "header";
-
-// JSX demo
-const element = <h1 className="header">This is JSX!</h1>;
-console.log(element);
-
-ReactDOM.render(
-  <div>
-    <MainContent />
-    {element}
-  </div>,
-
-  document.getElementById("root")
+const navbar = (
+  <nav>
+    <h1>Super Restaurant</h1>
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
 );
+
+ReactDOM.render(navbar, document.getElementById("root"));
