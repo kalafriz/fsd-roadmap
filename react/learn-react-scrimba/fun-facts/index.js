e9c52ca061c86440b4c5ad882f35185be238f2a3
kalafriz <kalafriz@bu.edu>
2024-04-24 09:27:38 -0400

    update component structure
    Split page into header, content components, added footer

--- react/learn-react-scrimba/fun-facts/index.js
+++ react/learn-react-scrimba/fun-facts/index.js
@@ -1,17 +1,52 @@
 const root = document.getElementById("root");
 
-const Facts = (
-  <div>
-    <img src="fact.png" alt="" height="50px" />
-    <h1>Fun Facts about Me!</h1>
-    <ul>
-      <li>I prefer chocolate over vanilla</li>
-      <li>Boston University is my alma mater</li>
-      <li>My favorite season is summer</li>
-      <li>I prefer having an Apple phone but a Windows computer</li>
-      <li>I dislike seafood</li>
-    </ul>
-  </div>
-);
+function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <img src="./fact.png" alt="Fact icon" height="50px" />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
 
-ReactDOM.render(Facts, root);
+function Facts() {
+  return (
+    <div>
+      <h1>Fun Facts about Me!</h1>
+      <ul>
+        <li>I prefer chocolate over vanilla</li>
+        <li>Boston University is my alma mater</li>
+        <li>My favorite season is summer</li>
+        <li>I prefer having an Apple phone but a Windows computer</li>
+        <li>I dislike seafood</li>
+      </ul>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <small>
+      2024 <a href="https://github.com/kalafriz">@kalafriz</a>. All rights
+      reserved.
+    </small>
+  );
+}
+
+function Page() {
+  return (
+    <>
+      <Header />
+      <Facts />
+      <Footer />
+    </>
+  );
+}
+
+ReactDOM.render(<Page />, root);
