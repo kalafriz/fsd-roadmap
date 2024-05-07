7844068b7e4285f356925cc64ee36f00fd60a3f4
kalafriz <kalafriz@bu.edu>
2024-05-07 03:55:08 -0400

    implement timezone select/update functionality

--- micromodal-time-select-app/main.js
+++ micromodal-time-select-app/main.js
@@ -1,9 +1,25 @@
+var DateTime = luxon.DateTime;
+
 const timeZoneElement = document.getElementById("timezone");
+const userTimeElement = document.getElementById("userTime");
+const userDateElement = document.getElementById("userDate");
+const timeZoneSelect = document.getElementById("timezone-offset");
 
-timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+timeZoneSelect.value = userTimeZone;
+timeZoneElement.innerHTML = userTimeZone;
 
-timeZoneElement.innerHTML = timeZone;
+function updateDateTime() {
+  // DateTime is immutable-- need to create new instance
+  tz = timeZoneSelect.value;
+  var rezoned = DateTime.local().setZone(tz);
 
-function updateTimeZone(tz) {
-  timeZoneElement.innerHTML = tz;
+  timeZoneElement.innerHTML = rezoned.zoneName;
+  userTimeElement.innerHTML = rezoned.toLocaleString(
+    DateTime.TIME_24_WITH_SECONDS
+  );
+  userDateElement.innerHTML = rezoned.toLocaleString(DateTime.DATE_HUGE);
 }
+updateDateTime();
+
+setInterval(updateDateTime, 1000);
