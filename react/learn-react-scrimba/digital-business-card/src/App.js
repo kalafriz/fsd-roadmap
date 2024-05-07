bc87327941273d9561702927958393aef60391ff
kalafriz <kalafriz@bu.edu>
2024-05-06 21:10:17 -0400

    update copy, links, pfp

--- react/learn-react-scrimba/digital-business-card/src/App.js
+++ react/learn-react-scrimba/digital-business-card/src/App.js
@@ -11,21 +11,21 @@ const root = document.getElementById("root");
   - Interests
   - Footer (social icons)  
   */
+
 function Info() {
   return (
     <div className="info">
       <img
         className="pfp"
-        src={require("./img/profile.jpg")}
+        src={require("./img/profile.png")}
         alt="Profile photo"
       />
-      <h1>Kirsten Alafriz</h1>
+      <h1>
+        <span className="firstname">Kirsten</span>{" "}
+        <span className="lastname">Alafriz</span>
+      </h1>
       <h3>Full Stack Developer</h3>
-      <a href="https://github.com/kalafriz">@kalafriz</a>
-      <div>
-        <button>Email</button>
-        <button>LinkedIn</button>
-      </div>
+      <p>kirstenalafriz@gmail.com</p>
     </div>
   );
 }
@@ -34,7 +34,15 @@ function About() {
   return (
     <div className="about">
       <h2>About</h2>
-      <p>Lorem ipsum blah blah blah blah blah.</p>
+      <p>
+        Software engineer with a passion for creating{" "}
+        <strong>human-centered design for big data</strong>. Diverse experience
+        portfolio in a wide range of applications{" "}
+        <strong>
+          from computational neuroscience research to formula racecar telemetry
+        </strong>
+        . Leader, excellent communicator, and always organized.{" "}
+      </p>
     </div>
   );
 }
@@ -43,7 +51,11 @@ function Interests() {
   return (
     <div className="interests">
       <h2>Interests</h2>
-      <p>I like to eat and breathe and and and and and and and and and</p>
+      <p>
+        I hold both a love for and education in neuroscience and the fine arts.
+        Always designing and structuring with <strong>your attention</strong> in
+        mind. Also a sterotypical cat-lover.{" "}
+      </p>
     </div>
   );
 }
@@ -51,21 +63,27 @@ function Interests() {
 function Footer() {
   return (
     <footer>
-      <a href="">LinkedIn</a>
-      <a href="">Github</a>
-      <a href="">LeetCode</a>
+      <button>
+        <a href="https://www.linkedin.com/in/kalafriz/">LinkedIn</a>
+      </button>
+      <button>
+        <a href="https://github.com/kalafriz">Github</a>
+      </button>
+      <button>
+        <a href="https://leetcode.com/u/kalafriz/">LeetCode</a>
+      </button>
     </footer>
   );
 }
 
 function App() {
   return (
-    <>
+    <div className="info-card">
       <Info />
       <About />
       <Interests />
       <Footer />
-    </>
+    </div>
   );
 }
 export default App;
