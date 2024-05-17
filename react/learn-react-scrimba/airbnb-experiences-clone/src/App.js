ddb19c0b02006cf63bf3a224ae2d805feb969e21
kalafriz <kalafriz@bu.edu>
2024-05-17 04:45:47 -0400

    add experience card

--- react/learn-react-scrimba/airbnb-experiences-clone/src/App.js
+++ react/learn-react-scrimba/airbnb-experiences-clone/src/App.js
@@ -3,12 +3,21 @@ import "./App.css";
 
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
+import Card from "./components/card";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Hero></Hero>
+      <Card
+        img="katie-zaferes.jpeg"
+        rating="4.9"
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      ></Card>
     </div>
   );
 }
---
+++ react/learn-react-scrimba/airbnb-experiences-clone/src/components/card.js
@@ -0,0 +1,24 @@
+import star from "../img/star.png";
+
+export default function Card(props) {
+  console.log(props);
+  return (
+    <>
+      <img
+        src={`${process.env.PUBLIC_URL}/img/${props.img}`}
+        alt="Experience Preview"
+        className="card--image"
+      />
+      <div className="card--stats">
+        <img src={star} alt="" className="card--star" />
+        <span>{` ${props.rating} `}</span>
+        <span className="grey">{`(${props.reviewCount}) • `}</span>
+        <span className="grey">{`${props.country}`}</span>
+      </div>
+      <p className="card--title">{props.title}</p>
+      <p>
+        <strong>{`From $${props.price} `}</strong>/ person
+      </p>
+    </>
+  );
+}
