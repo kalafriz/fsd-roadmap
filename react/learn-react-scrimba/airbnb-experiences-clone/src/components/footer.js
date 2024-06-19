e87669bd6e653b8ff6a149d7f5c89cf7b0d1dac3
kalafriz <kalafriz@bu.edu>
2024-06-19 03:13:32 -0400

    update footer links + styling

--- react/learn-react-scrimba/airbnb-experiences-clone/src/components/footer.js
+++ react/learn-react-scrimba/airbnb-experiences-clone/src/components/footer.js
@@ -1,8 +1,15 @@
 export default function Footer() {
   return (
     <footer>
-      <div>hi</div>
-      <div>bye</div>
+      <div>
+        <a href="https://github.com/kalafriz">@kalafriz</a> -{" "}
+        <a href="d">View Source Code</a>
+      </div>
+      <div>
+        <a href="https://scrimba.com/learn/learnreact">
+          Based on Bob Ziroll's AirBnB Experiences clone tutorial for Scrimba
+        </a>
+      </div>
     </footer>
   );
 }
