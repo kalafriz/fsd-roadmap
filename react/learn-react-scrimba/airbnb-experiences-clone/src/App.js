5e71574d9173aa4b4d835b581f64ea9da1ae74db
kalafriz <kalafriz@bu.edu>
2024-06-19 03:03:50 -0400

    add data mapping

--- react/learn-react-scrimba/airbnb-experiences-clone/src/App.js
+++ react/learn-react-scrimba/airbnb-experiences-clone/src/App.js
@@ -4,20 +4,29 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
 import Card from "./components/card";
+import Footer from "./components/footer";
+
+import data from "./data"; // card data
 
 function App() {
+  const cards = data.map((card) => {
+    return (
+      <Card
+        img={card.coverImg}
+        rating={card.stats.rating}
+        reviewCount={card.stats.reviewCount}
+        country={card.location}
+        title={card.title}
+        price={card.price}
+      ></Card>
+    );
+  });
   return (
     <div className="App">
       <Navbar></Navbar>
       <Hero></Hero>
-      <Card
-        img="katie-zaferes.jpeg"
-        rating="4.9"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      ></Card>
+      <div className="cards--grid">{cards}</div>
+      <Footer></Footer>
     </div>
   );
 }
