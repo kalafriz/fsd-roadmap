8a18bb24bcda221758d72f84b26b24030f490f41
kalafriz <kalafriz@bu.edu>
2024-07-31 05:59:07 -0400

    add pin icon + attribution

--- react/learn-react-scrimba/travel-journal/src/components/Footer.js
+++ react/learn-react-scrimba/travel-journal/src/components/Footer.js
@@ -1,3 +1,13 @@
 export default function Footer() {
-  return <footer>footer yay</footer>;
+  return (
+    <footer>
+      footer yay
+      <a
+        href="https://www.flaticon.com/free-icons/location"
+        title="location icons"
+      >
+        Location icons created by kmg design - Flaticon
+      </a>
+    </footer>
+  );
 }
