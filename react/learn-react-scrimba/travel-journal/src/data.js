213b7a50d9695f95a2aed27850dc9ba9612075e0
kalafriz <kalafriz@bu.edu>
2024-07-31 05:58:46 -0400

    populate sample data

---
+++ react/learn-react-scrimba/travel-journal/src/data.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://maps.app.goo.gl/j1bYMM7rSZSAHs4a6",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description: "Today we hiked Mount Fuji, it was very beautiful.",
+    imageUrl:
+      "https://res.cloudinary.com/jnto/image/upload/w_750,h_503,fl_lossy,f_auto/v1531981666/fujiguide/SG010_6",
+  },
+];
