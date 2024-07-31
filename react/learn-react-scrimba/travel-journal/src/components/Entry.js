b50852b9e6719197c22c33ef771399881debbfe0
kalafriz <kalafriz@bu.edu>
2024-07-31 09:55:25 -0400

    styling entry cards

--- react/learn-react-scrimba/travel-journal/src/components/Entry.js
+++ react/learn-react-scrimba/travel-journal/src/components/Entry.js
@@ -2,19 +2,21 @@ import pin from "./icons/pin.png";
 export default function Entry(props) {
   return (
     <div className="entry">
-      <img src={`${props.img}`} alt={props.title} />
+      <img src={`${props.img}`} alt={props.title} className="entry--img" />
       <div className="entry--data">
         <span>
-          <h3>{props.title}</h3>
-          <a href={props.googleMapsUrl}>
-            <img src="pin" alt="" className="entry--pin" />
-          </a>
+          <h3>
+            {props.title}
+            <a href={props.googleMapsUrl}>
+              <img src={pin} alt="" className="entry--pin" />
+            </a>
+          </h3>
         </span>
         <h5>{props.location}</h5>
         <p>
           {props.startDate} - {props.endDate}
         </p>
-        <p>{props.description}</p>
+        <p className="entry--description">{props.description}</p>
       </div>
     </div>
   );
