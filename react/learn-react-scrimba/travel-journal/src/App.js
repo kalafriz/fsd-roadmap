e9988a0218ae12ff5f570beb2578f2f1f9e52921
kalafriz <kalafriz@bu.edu>
2024-07-31 05:23:09 -0400

    initial commit

---
+++ react/learn-react-scrimba/travel-journal/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
