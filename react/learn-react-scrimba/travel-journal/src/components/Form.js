9303e253e2d13b28b8647c60e70663ebe07efcd1
kalafriz <kalafriz@bu.edu>
2024-08-02 09:02:22 -0400

    update: image upload encoding + form clear

--- react/learn-react-scrimba/travel-journal/src/components/Form.js
+++ react/learn-react-scrimba/travel-journal/src/components/Form.js
@@ -6,21 +6,44 @@ export default function Form({ addEntry }) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    alert("form submitted!");
     const formData = new FormData(event.target);
+    console.log(formData);
     const formJSON = Object.fromEntries(formData.entries());
+    formJSON.imageUrl = file;
     console.log(formJSON);
     addEntry(formJSON);
+    setFile(upload);
+    document.getElementById("form").reset();
   }
 
   function handleChange(event) {
     console.log(event.target.files);
-    setFile(URL.createObjectURL(event.target.files[0]));
+    if (event.target.files && event.target.files.length) {
+      /*
+      const imgUrl = URL.createObjectURL(event.target.files[0]);
+      setFile(imgUrl);
+      console.log("imgURL:");
+      console.log(imgUrl);
+      */
+
+      // encode image to base64 so it can be saved to localStorage
+      // following https://fabiorosado.dev/blog/upload-images-without-backend/#using-localstorage-to-save-the-image-and-display-it-on-each-visit
+      const imgFile = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.readAsDataURL(imgFile);
+      reader.onload = () => {
+        const result = String(reader.result);
+        if (result) {
+          setFile(result);
+        }
+      };
+    }
   }
 
   return (
     <div>
-      <form onSubmit={handleSubmit} className="form--card">
+      <form onSubmit={handleSubmit} id="form" className="form--card">
         <div className="form--dropzone">
           <input
             name="imageUrl"
@@ -30,19 +53,13 @@ export default function Form({ addEntry }) {
             className="form--fileUpload"
             required
           />
-          <div
-            className="form--img"
-            style={{
-              background: `url(${file}) no-repeat center`,
-              backgroundSize: "300px",
-            }}
-          ></div>
+          <img src={file} alt="" className="form--img" />
         </div>
         <span className="form--data">
           <input
             name="title"
             type="text"
-            placeholder="Title"
+            placeholder="New Entry"
             className="h3"
             required
           />
